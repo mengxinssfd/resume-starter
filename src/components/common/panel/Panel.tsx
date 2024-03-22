84e3060976b67041870a49f9ea8fbc2b089d20b6
dyh_a <xinzon32177@163.com>
2024-03-23 00:08:37 +0800

    feat: 为 Panel 组件的 title 添加锚点跳转功能

--- src/components/common/panel/Panel.tsx
+++ src/components/common/panel/Panel.tsx
@@ -7,7 +7,14 @@ export const Panel: FC<
   return (
     <section className={getClassNames(style.root, className)}>
       <div className="panel-header">
-        <h2 className="panel-title">{title}</h2>
+        <h2 className="panel-title">
+          <a
+            className="anchor"
+            href={'/#' + title}
+            id={(title || '').toString()}>
+            {title}
+          </a>
+        </h2>
       </div>
       <div className="panel-body">{children}</div>
     </section>
