798ddcbe5db774fdcfa5d6cc44dafed907f210d3
dyh_a <xinzon32177@163.com>
2024-04-11 05:53:46 +0800

    refactor: views 目录下的各种 panels 移动到 views/panels 目录下

---
+++ src/views/panels/index.ts
@@ -0,0 +1,6 @@
+export * from './skill';
+export * from './experience';
+export * from './github';
+export * from './personal-project';
+export * from './evaluation';
+export * from './personal-advantage';
