e074b86262f96b09a38950806a8a3129747c50d0
dyh_a <xinzon32177@163.com>
2024-06-17 18:08:56 +0800

    fix: 修复 header view 中的文件 input 会在点击并取消后不会清理的问题

--- src/views/header/Header.tsx
+++ src/views/header/Header.tsx
@@ -1,12 +1,13 @@
-import { FC, ReactElement, useContext } from 'react';
+import React, { FC, ReactElement, useContext, useRef } from 'react';
 import style from './header.module.scss';
 import { PDFDocument, StandardFonts } from 'pdf-lib';
-import { download, readFile, createHtmlElement } from '@tool-pack/dom';
+import { download, readFile } from '@tool-pack/dom';
 import { getClassNames } from '@tool-pack/basic';
 import { SettingContext } from '@/context';
 
 export const Header: FC = (): ReactElement => {
   const { lastModified } = useContext(SettingContext);
+  const pdfPageInputRef = useRef<HTMLInputElement>(null);
   return (
     <div className={getClassNames(style.root, 'print-hidden')}>
       <div className="left">
@@ -17,6 +18,13 @@ export const Header: FC = (): ReactElement => {
         <button className="generate-pdf btn" onClick={addPDFPage}>
           为 PDF 文件生成页码
         </button>
+        <input
+          onChange={onPDFPageChange}
+          className="pdf-page-input"
+          accept="application/pdf"
+          ref={pdfPageInputRef}
+          type="file"
+        />
       </div>
       <div className="right">
         <p className="last-modified">最后更新于 {lastModified}</p>
@@ -27,26 +35,24 @@ export const Header: FC = (): ReactElement => {
   function generatePdf() {
     window.print();
   }
+  async function onPDFPageChange(
+    e: React.ChangeEvent<HTMLInputElement>,
+  ): Promise<void> {
+    const input = e.target;
+    if (!input) return;
+
+    const file = input.files?.[0];
+    if (!file) return;
+
+    input.value = '';
+    const r = await readFile(file, 'readAsArrayBuffer');
+    if (!r || typeof r === 'string') return;
+
+    const u8a = await modifyPDF(new Uint8Array(r));
+    download(file.name, new Blob([u8a]));
+  }
   function addPDFPage() {
-    const input = createHtmlElement('input', {
-      props: {
-        type: 'file',
-        accept: 'application/pdf',
-        onchange: async () => {
-          const file = input.files?.[0];
-          input.remove();
-          if (!file) return;
-
-          const r = await readFile(file, 'readAsArrayBuffer');
-          if (!r || typeof r === 'string') return;
-
-          const u8a = await modifyPDF(new Uint8Array(r));
-          download(file.name, new Blob([u8a]));
-        },
-      },
-      parent: document.body,
-    });
-    input.click();
+    pdfPageInputRef.current!.click();
   }
   async function modifyPDF(u8a: Uint8Array): Promise<Uint8Array> {
     const pdfDoc = await PDFDocument.load(u8a);
